06c0856a476198b7c8d40c2b204ce6890f779592
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2023-04-03 21:00:44 -0300

    feat: adicionao react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<p>Página Inicial</p>} path={"/pagina-inicial"} />
+      <Route element={<Navigate to={"pagina-inicial"} />} path={"*"} />
+    </Routes>
+  );
+};
